ca360ecc0639e125ad90cf4adedd85c984803907
devdiagon <fredericktimo@gmail.com>
2026-05-11 09:59:46 -0500

    feat: render sent message

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,2 +1,33 @@
+// Iniciar servidor de Socket
 const socket = io();
 
+ const send = document.querySelector('#send-message');
+ const allMessages = document.querySelector('#all-messages');
+
+ send.addEventListener('click', () => {
+  // Capturar el valor del mensaje escrito
+  const message = document.querySelector('#message').value;
+
+  // Emitir un evento llamado "message" con el contenido de la variable message
+  socket.emit("message", message);
+ });
+
+ socket.on('message', ({ user, message }) => {
+  const msg = document.createRange().createContextualFragment(`
+    <div class="message">
+      <div class="image-container">
+        <img src="/img/seal.png" alt="" />
+      </div>
+      <div class="message-body">
+        <div class="user-info">
+          <span class="username">${user}</span>
+          <span class="time">10:30 AM</span>
+          <p>
+            ${message}
+          </p>
+        </div>
+      </div>
+    </div>
+    `);
+    allMessages.append(msg);
+ });
--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -4,6 +4,15 @@ module.exports = (httpServer) => {
 
   // Encender la contectividad del socket
   io.on('connection', (socket) => {
-    console.log(`ID: ${socket.id}`);
+    // console.log(`ID: ${socket.id}`);
+
+    // Llamar al evento 'message' y obtener sus parámetros (en este caso con la variable llamada message)
+    socket.on('message', message => {
+      io.emit('message', {
+        user: 'Fredo',
+        message,
+      });
+    });
+
   });
 };
